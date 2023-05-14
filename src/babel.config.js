6284918baee47e080165bd82716529fc53d1e055
Guilherme Martins Garcia <plaice-smiths00@icloud.com>
2023-05-14 00:56:22 -0300

    chore  Adding build with Babel

---
+++ src/babel.config.js
@@ -0,0 +1,53 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {compilerOptions} = require('./tsconfig.json');
+
+function removeSlash(string) {
+  return string.replace('/*', '');
+}
+
+function convertToBabelAliases(tsConfigPaths) {
+  const aliases = {};
+
+  if (!tsConfigPaths) {
+    return aliases;
+  }
+
+  for (const [key, value] of Object.entries(tsConfigPaths)) {
+    if (value.length > 1) {
+      throw Error(
+        '[Babel.config]: Conversion from multiple path alias not supported',
+      );
+    }
+    aliases[`${removeSlash(key)}`] = `./${removeSlash(value[0])}`;
+  }
+
+  return aliases;
+}
+
+const babelConfig = {
+  sourceMaps: 'inline',
+  presets: [
+    '@babel/preset-typescript',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: true,
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+        alias: convertToBabelAliases(compilerOptions?.paths),
+      },
+    ],
+  ],
+  ignore: [/node_modules/, "**/*.spec.ts", "**/*.mocks.ts"],
+};
+
+module.exports = babelConfig;
