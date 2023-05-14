439651f1e42899a4f10323e475ed7e7efd802052
Guilherme Martins Garcia <plaice-smiths00@icloud.com>
2023-05-14 11:42:25 -0300

    fix  Removing unused imports

--- src/CounterService/CounterRepository/CounterRepository.mocks.ts
+++ src/CounterService/CounterRepository/CounterRepository.mocks.ts
@@ -1,7 +1,5 @@
 import {Connection} from 'mysql2/promise';
 import {
-  getHitCountResult,
-  getSelectIdResult,
   HitCountResult,
   SelectIdResult,
 } from '@CounterService/CounterRepository/CounterRepository.types';
