8d7940a2b94b4e1780262a8c8c0c16173ae784b3
Guilherme Martins Garcia <plaice-smiths00@icloud.com>
2023-08-04 10:51:53 -0300

    chore  Add `blocked terms` check to pre-commit

---
+++ src/.scripts/checkCommitBlockedTerms.sh
@@ -0,0 +1,36 @@
+# !/bin/sh
+
+# Checks the git diff cache for blocked terms.
+#
+# To bypass this hook after all other checks are passing, use the "--no-verify"
+# parameter when committing.
+
+checkCommitBlockedTerms() {
+  # Redirect output to stderr.
+  exec 1>&2
+
+  declare -a blockedTerms=(
+    "TODO"
+    "//"
+  )
+
+  changeToScriptDir() { cd $(dirname $0); }
+  changeToScriptDir;
+
+  local foundError=false
+
+  for term in "${blockedTerms[@]}"
+  do
+    if ! ./checkCommitForTerm.sh $term; then
+      foundError=true
+    fi
+  done
+
+  if [ "$foundError" = true ]; then
+    exit 1
+  fi
+
+  echo  Found no blocked terms
+}
+
+checkCommitBlockedTerms;
---
+++ src/.scripts/checkCommitForTerm.sh
@@ -0,0 +1,31 @@
+# !/bin/sh
+
+# Checks the git diff cache for the provided term.
+
+checkCommitForTerm() {
+  # Redirect output to stderr.
+  exec 1>&2
+  
+  getGitCache() { git diff --cached; }
+  filterAdditions() { grep '^+' $1; }
+  ignoreFilenames() { grep -v '+++ b/' $1; }
+  ignoreFirstChar() { cut -c 2- $1; }
+  findBlockedTerm() { grep -i $1 $2; }
+  printError() {
+    # Define colors
+    local RED='\033[0;31m'
+    local NO_COLOR='\033[0m'
+
+    printf "${RED}error:${NO_COLOR} ${1}"
+  }
+  
+  local FOUND_TERM=$(getGitCache | filterAdditions | ignoreFilenames | ignoreFirstChar | findBlockedTerm $1)
+  
+  if [[ $FOUND_TERM ]]; then
+    printError "Found '${1}' in attempted commit.\n"
+    printError "Please remove all occurrences of '${1}' before committing.\n\n"
+    exit 1
+  fi
+}
+
+checkCommitForTerm $1;
